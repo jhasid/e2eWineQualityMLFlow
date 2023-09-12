b38c01e0b79dda9e5cdca8d3315f30e24fe05935
Sid <siddharth.jha30@gmail.com>
2023-09-12 19:20:58 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from e2eWineQualityMLFlow import logger
 from e2eWineQualityMLFlow .pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from e2eWineQualityMLFlow .pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from e2eWineQualityMLFlow .pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from e2eWineQualityMLFlow .pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -36,6 +37,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e   
 
 
 
