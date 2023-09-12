2d61c942cd34e6bf7eead230bee5213dd34435af
Sid <siddharth.jha30@gmail.com>
2023-09-12 18:31:25 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from e2eWineQualityMLFlow import logger
 from e2eWineQualityMLFlow .pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from e2eWineQualityMLFlow .pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from e2eWineQualityMLFlow .pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -19,6 +20,17 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
